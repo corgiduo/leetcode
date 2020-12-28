2a10a42dbc5ce7ef74ccef98ba64a51193951063
liuqiheng <qiheng.liu@outlook.com>
2020-12-28 19:50:20 +0800

    分离常用数据结构

--- src/Main.java
+++ src/Main.java
@@ -10,39 +10,3 @@ public class Main {
     }
 
 }
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode(int x) {
-        val = x;
-    }
-}
-
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) { val = x; }
-}
-
-class Node {
-    public int val;
-    public List<Node> neighbors;
-
-    public Node() {
-        val = 0;
-        neighbors = new ArrayList<>();
-    }
-
-    public Node(int _val) {
-        val = _val;
-        neighbors = new ArrayList<>();
-    }
-
-    public Node(int _val, ArrayList<Node> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-}
