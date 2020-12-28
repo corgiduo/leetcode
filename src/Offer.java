015deabd5227675c975fad48ed23f31e4ee648f0
liuqiheng <qiheng.liu@outlook.com>
2020-12-28 19:19:07 +0800

    05. 替换空格

--- src/Offer.java
+++ src/Offer.java
@@ -39,6 +39,19 @@ public class Offer {
         return false;
     }
 
+    //05. 替换空格
+    public String replaceSpace(String s) {
+        StringBuffer sb = new StringBuffer();
+        for (char c : s.toCharArray()) {
+            if (c == ' ') {
+                sb.append("%20");
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
     //11. 旋转数组的最小数字
     public int minArray(int[] numbers) {
         if (numbers[0] < numbers[numbers.length - 1]) return numbers[0];
